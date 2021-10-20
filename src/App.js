a887991f237946187041168f2ed27cf5fbac0173
JuanPineda1985 <71187053+JuanPineda1985@users.noreply.github.com>
2021-10-20 18:38:01 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import './App.css';
+import Menu from './Menu';
+import List from './List';
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      books:[
+        {id:0, rating: 4, title: 'Señor de los Anillos, la Comunidad del Anillo', image: 'libro01.jpg'},
+        {id:1, rating: 3, title: 'Señor de los Anillos, las 2 Torres', image: 'libro02.jpg'},
+        {id:2, rating: 5, title: 'Señor de los Anillos, el Retorno del Rey', image: 'libro03.jpg'},
+        {id:3, rating: 5, title: 'Hombre lobo el Apocalipsis', image: 'libro04.jpg'},
+        {id:4, rating: 5, title: 'Vampiro la Mascarada', image: 'libro05.jpg'}
+      ],
+      copyBooks: []
+    };
+
+    this.onSearch = this.onSearch.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.remove = this.remove.bind(this);
+    this.updateRating = this.updateRating.bind(this);
+  }
+
+  initBooks(){
+    //this.setState({copyBooks: [...this.state.books]});
+    this.setState((state,props) => ({
+      copyBooks: [...state.books]
+    }));
+  }
+
+  componentDidMount(){
+    this.initBooks();
+  }
+
+  onSearch(query){
+    if(query === ''){
+      this.setState({copyBooks: [...this.state.books]});
+    }else{
+
+      const temp = [...this.state.books];
+      var res = [];
+      temp.forEach(item =>{
+        if(item.title.toLowerCase().indexOf(query) > -1){
+          res.push(item);
+        }
+      });
+    
+      this.setState({copyBooks: [...res]});
+    }
+  }
+
+  addItem(item){
+    var temp = [...this.state.books];
+    const id = temp[temp.length-1].id + 1;
+    item['id'] = id;
+    temp.push(item);
+    this.setState({books: [...temp]});
+    this.initBooks();
+  }
+
+  remove(id){
+    var temp = [...this.state.books];
+    const res = temp.filter(item => item.id != id);
+    this.setState({books: [...res]});
+    this.initBooks();
+  }
+
+  updateRating(item){
+    var temp = [...this.state.books];
+    const index = temp.findIndex(x => x.id === item.id);
+    temp[index].title = item.title;
+    temp[index].image = item.image;
+    temp[index].rating = item.rating;
+
+    this.setState({books: [...temp]});
+    this.initBooks();
+  }
+
+  render(){
+    return (
+      <div className="app">
+        <Menu title="Book-Store" onsearch={this.onSearch} onadd={this.addItem} />
+        <List className="list" items={this.state.copyBooks} onremove={this.remove} onupdaterating={this.updateRating} />
+      </div>
+    );
+  }
+}
+
+export default App;
